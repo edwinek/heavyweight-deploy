346776aa74912d86f49880fe760af4bc62a63306
eddie <eddie@example.com>
2016-11-15 21:36:55 +0000

    Removing BASH script workarounds

--- deploy.sh
+++ deploy.sh
@@ -25,7 +25,6 @@ function create() {
 	for DIR_TO_CREATE in builder getter deployer
 	do
 		mkdir $DIR_TO_CREATE
-		touch $DIR_TO_CREATE/Dockerfile
 	done
 }
 
@@ -33,26 +32,10 @@ cleanup_containers
 cleanup_files
 create
 
-FILENAME=git-clone-$(date -u +"%Y%m%d%H%M%S").sh
-LOCAL_FILE=getter/$FILENAME
-
-touch $LOCAL_FILE
-chmod +x $LOCAL_FILE
-
-cat > "$LOCAL_FILE" <<EOL
-#!/bin/bash
-git clone https://github.com/edwinek/heavyweight.git
-EOL
-touch getter/Dockerfile
-
 cat > getter/Dockerfile <<EOL
 FROM debian:8.4
 
 RUN apt-get -y update && apt-get install -y git
-ADD $FILENAME . 
-RUN ./$FILENAME
-WORKDIR heavyweight
-RUN git archive master > /tmp/src.tar
 EOL
 
 cat > builder/Dockerfile <<EOL
@@ -69,21 +52,13 @@ FROM tomcat:8.0.33-jre7
 
 RUN apt-get -y update && apt-get install -y mongodb
 ADD heavyweight.war /usr/local/tomcat/webapps/heavyweight.war
-ADD go.sh /opt/go.sh
-CMD ["/opt/go.sh"]
-EOL
-
-touch deployer/go.sh
-chmod +x deployer/go.sh
-
-cat > deployer/go.sh <<EOL
-#! /bin/bash
-service mongodb start && catalina.sh run
 EOL
 
 
 docker build -t getter_image getter/.
 docker run --name getter_container -d getter_image tail -f /dev/null
+docker exec -ti getter_container git clone http://www.github.com/edwinek/heavyweight /opt/src/heavyweight
+docker exec -ti getter_container bash -c "cd /opt/src/heavyweight && git archive -o /tmp/src.tar master"
 docker cp getter_container:/tmp/src.tar builder/src.tar
 docker stop getter_container 
 
@@ -96,6 +71,8 @@ docker build -t deployer_image deployer/.
 
 cleanup_files
 
-docker run --name deployer_container -it -p 8888:8080 deployer_image
+docker run --name deployer_container -d -p 8888:8080 deployer_image tail -f /dev/null
+docker exec -ti deployer_container bash -c "service mongodb start && catalina.sh run"
+docker stop deployer_container
 
 cleanup_containers
